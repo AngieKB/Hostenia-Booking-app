ffafc34ca19fe93b6960b6e4f3b32b14a11c2a93
AngieKB <angiek.bonillam@uqvirtual.edu.co>
2025-11-11 20:33:17 -0500

    Validaciones de contraseña

--- src/main/java/co/edu/uniquindio/Application/DTO/Usuario/ChangePasswordDTO.java
+++ src/main/java/co/edu/uniquindio/Application/DTO/Usuario/ChangePasswordDTO.java
@@ -1,9 +1,15 @@
 package co.edu.uniquindio.Application.DTO.Usuario;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record ChangePasswordDTO(
         @NotBlank String oldPassword,
-        @NotBlank String newPassword
+        @NotBlank @Size(min=8, message = "La contraseña debe tener al menos 8 caracteres")
+        @Pattern(
+                regexp = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).*$",
+                message = "La contraseña debe contener al menos una letra mayúscula, una letra minúsucla, y un número"
+        ) String newPassword
 ) {
 }
--- src/main/java/co/edu/uniquindio/Application/DTO/Usuario/ResetPasswordDTO.java
+++ src/main/java/co/edu/uniquindio/Application/DTO/Usuario/ResetPasswordDTO.java
@@ -2,9 +2,15 @@ package co.edu.uniquindio.Application.DTO.Usuario;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record ResetPasswordDTO(
         @Email String email,
         @NotBlank String verificationCode,
-        @NotBlank String newPassword
+        @NotBlank @Size(min=8, message = "La contraseña debe tener al menos 8 caracteres")
+        @Pattern(
+                regexp = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).*$",
+                message = "La contraseña debe contener al menos una letra mayúscula, una letra minúsucla, y un número"
+        ) String newPassword
 ){}
